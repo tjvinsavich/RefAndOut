546d366e1fa5a3c7d467a654971cb063a5846193
Whit Stroup <email@example.com>
2021-06-25 13:14:30 -0500

    yes

---
+++ RefandOut/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RefandOut
+{
+    class Program
+    {
+        //TODO impliment this ref method by returning  a string and mulitplying the parameter. So it will have two outputs but only returns the string.
+        //TODO Call the method in the main and make sure the method changes the number of a initialized variable.
+        public static string TimesFive(ref int x)
+        {
+  
+        }
+
+        //TODO create method that uses the out keyword. Call the method in the main.
+  
+        static void Main(string[] args)
+        {
+        
+        }
+
+    }
+}
