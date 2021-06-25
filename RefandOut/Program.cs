6b9fc009cbf490c19836de6c03f1f525bb49650d
TJ Vinsavich <tjvinsavich@gmail.com>
2021-06-25 14:04:45 -0500

    finished TODOs

--- RefandOut/Program.cs
+++ RefandOut/Program.cs
@@ -8,13 +8,36 @@ namespace RefandOut
         //TODO Call the method in the main and make sure the method changes the number of a initialized variable.
         public static string TimesFive(ref int x)
         {
-  
+            x = x * 5;
+
+            return $"{x / 5} times 5 is {x}";
         }
 
         //TODO create method that uses the out keyword. Call the method in the main.
+        public static string HalfOfNum(double y, out double number)
+        {
+            number = y / 2;
+
+            return $"Half of {y} is {number}";
+        }
   
         static void Main(string[] args)
         {
+            Console.WriteLine("Ref");
+            Console.WriteLine("-----------------------");
+
+            int x = 10;
+            Console.WriteLine($"Initial x value: {x}");
+            Console.WriteLine(TimesFive(ref x));
+            Console.WriteLine($"Final x value: {x}\n");
+
+            Console.WriteLine("Out");
+            Console.WriteLine("-----------------------");
+
+            double number;
+            Console.WriteLine("No value yet for number");
+            Console.WriteLine(HalfOfNum(50, out number));
+            Console.WriteLine($"Final number value: {number}");
         
         }
 
